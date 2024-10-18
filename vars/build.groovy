a5cf9c4efd915c48cc68900c5e92e2b3e1418270
Sharuquee <160309748+Sharuquee@users.noreply.github.com>
2024-10-18 16:55:18 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 
 def call(String Project, String Imagetag, String dockerhubuser){
-  sh "docker build -t ${"dockerhubuser"}/${"project"}:${"Imagetag"}
+  sh "docker build -t ${dockerhubuser}/${project}:${Imagetag} ."
 }
