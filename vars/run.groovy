5ff20aa1428ca3a71d602465d0ea3ea2009bdd8d
Sharuquee <160309748+Sharuquee@users.noreply.github.com>
2024-11-09 20:28:07 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,3 +1,11 @@
-def call(){
-  sh " docker-compose up -d "
-}
+def call() {
+    try {
+        // Run docker-compose to start containers in detached mode
+        sh 'docker-compose up -d'
+    } // catch (Exception e) {
+        // Catch the error, print the message, and mark the build as failed
+        // echo "Docker Compose failed: ${e.getMessage()}"
+        // currentBuild.result = 'FAILURE' // Mark the build as failure
+        // throw e // Re-throw the error to fail the build
+    // }
+// }
