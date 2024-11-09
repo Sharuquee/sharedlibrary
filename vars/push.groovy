4bed5ab7951784691fd85cfc62d8e9327a5a427f
Sharuquee <160309748+Sharuquee@users.noreply.github.com>
2024-11-09 20:05:27 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def call(String credentialsid String toolname String projectname String dockerhubuser){
+def call(String projectname String dockerhubuser){
   withDockerRegistry(credentialsid: docker, toolname: Docker){
     sh " docker image tag notes-app sharuquee/notes-app:$BUILD_ID "
     sh " docker push sharuquee/notes-app:$BUILD_ID "
